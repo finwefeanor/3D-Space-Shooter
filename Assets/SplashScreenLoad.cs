93af24d31af1b7c7ebb7117e9f085b1a0d4a855d
UMUT YILDIRIM <yolgezerestel@gmail.com>
2018-03-11 17:24:22 +0300

    First Commit

---
+++ Assets/SplashScreenLoad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //SceneManager.LoadScene(0);
+        Invoke("LoadSplashLevel", 2f);
+    }
+	
+    void LoadSplashLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
